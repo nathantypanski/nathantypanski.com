d1eb63583ea3c0a69e2093a9f02c2bd706df6b36
ndt <mail@nathantypanski.com>
2025-05-07 01:39:20 -0400

    make stack work again

--- shell.nix
+++ shell.nix
@@ -1,8 +1,15 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ee01de29d2f58d56b1be4ae24c24bd91c5380cea.tar.gz") {}
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-23.11.tar.gz") {}
+  # Or use a specific commit:
+  # { pkgs ? import (fetchTarball {
+  #     url = "https://github.com/NixOS/nixpkgs/archive/YOUR_CHOSEN_COMMIT_HASH.tar.gz";
+  #     # You'll need to get the sha256 for that tarball, e.g., using nix-prefetch-url
+  #     sha256 = "YOUR_SHA256_HASH";
+  #   }) {}
 }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    pkg-config
     zlib
     git
     gcc
